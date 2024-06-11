f5d69ae27daa5e4a677aed8f82c32496ca460ebd
Administrator <admin@example.com>
2024-06-11 13:32:17 +0200

    Ajoutez des fichiers projet.

---
+++ Web/Program.cs
@@ -0,0 +1,34 @@
+using Web.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddScoped<UserService>();
+builder.Services.AddScoped<ParcService>();
+builder.Services.AddScoped<DeviceService>();
+builder.Services.AddScoped<RoomService>();
+builder.Services.AddScoped<AuthenticationService>();
+builder.Services.AddScoped<HttpClient>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
